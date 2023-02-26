3d096ede2d3ddef758e1cc1f286d5e7c0135d6b5
girija <girija.pradhan422@gmail.com>
2023-02-26 14:09:55 +0530

    sec commit

--- src/main/java/com/javatechie/aws/cicd/example/Order.java
+++ src/main/java/com/javatechie/aws/cicd/example/Order.java
@@ -1,14 +1,5 @@
 package com.javatechie.aws.cicd.example;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 public class Order {
 
     private int id;
